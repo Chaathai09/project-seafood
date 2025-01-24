870c48acc2fb1978c5b719cc9891acecb477b070
GUEST-1001 <nawatgame@live.com>
2025-01-24 15:48:00 +0700

    citybomb basic move

--- Assets/Script/PlayerCityBomb.cs
+++ Assets/Script/PlayerCityBomb.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCityBomb : MonoBehaviour
@@ -7,23 +9,25 @@ public class PlayerCityBomb : MonoBehaviour
     [SerializeField] GameObject playerObj;
     [SerializeField] float speed;
     [SerializeField] Vector2 startPoint;
+    [SerializeField] int bombNum;
+    int useBombNum;
+    [SerializeField] TMP_Text bombText;
+    [SerializeField] Action gameState;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerObj.transform.position = startPoint;
+        useBombNum = bombNum;
+        gameState = WaitForStart;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
-    }
-
-    private void FixedUpdate()
-    {
-        MiniGameRun();
+        gameState();
+        bombText.text = useBombNum.ToString();
+        Debug.Log(gameState);
     }
 
     void MiniGameRun()
@@ -31,7 +35,32 @@ public class PlayerCityBomb : MonoBehaviour
         if (playerObj.transform.position.x >= 10f)
         {
             playerObj.transform.position = new Vector2(startPoint.x, playerObj.transform.position.y - 1f);
+            useBombNum = bombNum;
         }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            DropBomb();
+        }
+
         playerObj.transform.Translate((Vector2.right * speed) * Time.deltaTime);
     }
+
+    void WaitForStart()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            ChangeState(MiniGameRun);
+    }
+
+    void ChangeState(Action state)
+    {
+        gameState = state;
+    }
+
+    void DropBomb()
+    {
+        //make bomb destroy building
+        useBombNum -= 1;
+    }
+
 }
