bae806e6bc415c084231e3cd10c164f8e44518ec
GUEST-1001 <nawatgame@live.com>
2025-01-24 23:32:10 +0700

    make clamp on SI minigame

--- Assets/Script/SpaceImpact/SIPlayer.cs
+++ Assets/Script/SpaceImpact/SIPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class SIPlayer : MonoBehaviour
 {
     GameObject siPlayer;
     [SerializeField] float speed;
+    Action gameState;
+    Transform tempPos;
 
     private void Awake()
     {
@@ -14,12 +17,42 @@ public class SIPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameState = WaitForStart;
     }
 
     // Update is called once per frame
     void Update()
     {
-        siPlayer.transform.Translate(new Vector2((Input.GetAxisRaw("Horizontal") * speed) * Time.deltaTime, (Input.GetAxisRaw("Vertical") * speed) * Time.deltaTime));
+        tempPos = siPlayer.transform;
+
+        tempPos.Translate(new Vector2((Input.GetAxisRaw("Horizontal") * speed) * Time.deltaTime, (Input.GetAxisRaw("Vertical") * speed) * Time.deltaTime));
+
+        tempPos.position = new Vector2(Math.Clamp(tempPos.position.x, -8f, 8f), Math.Clamp(tempPos.position.y, -4f, 4f));
+
+        siPlayer.transform.position = tempPos.position;
+    }
+
+    void MiniGameRun()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            ShotAmmo();
+        }
+    }
+
+    void WaitForStart()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            ChangeState(MiniGameRun);
+    }
+
+    void ChangeState(Action state)
+    {
+        gameState = state;
+    }
+
+    void ShotAmmo()
+    {
+
     }
 }
