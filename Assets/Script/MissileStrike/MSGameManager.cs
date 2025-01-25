186b815844ca6414b38fcd892a73a4aa4a5ec9b0
GUEST-1001 <nawatgame@live.com>
2025-01-25 11:42:54 +0700

    Missile Strike add Timer

--- Assets/Script/MissileStrike/MSGameManager.cs
+++ Assets/Script/MissileStrike/MSGameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,9 +11,11 @@ public class MSGameManager : MonoBehaviour
     [SerializeField] float spawnDelay;
     int msScore = 0;
     [SerializeField] int getScore;
-    [SerializeField] TMP_Text msScoreText, msHPText;
+    [SerializeField] TMP_Text msScoreText, msHPText, msTimerText;
     [SerializeField] int playerHP;
     [SerializeField] MSPlayerCon playerCon;
+    [SerializeField] float timeLimit;
+    Action gameState;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,25 +27,45 @@ public class MSGameManager : MonoBehaviour
     {
         msScoreText.text = msScore.ToString();
         msHPText.text = playerHP.ToString();
+        msTimerText.text = Mathf.Ceil(timeLimit).ToString();
+        if (gameState != null)
+        {
+            gameState();
+        }
+    }
+
+    void TimeCount()
+    {
+        timeLimit -= Time.deltaTime;
+        if (timeLimit <= 0f)
+        {
+            playerCon.EndGame();
+        }
     }
 
     public void StartInvoke()
     {
         InvokeRepeating("SpawnMeteor", 0f, spawnDelay);
+        gameState = TimeCount;
     }
     public void StopInvoke()
     {
         CancelInvoke();
+        gameState = null;
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Meteor"))
         {
             obj.GetComponent<MeteorCon>().speed = 0f;
+        }
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Missile"))
+        {
+            obj.GetComponent<MissileCon>().speed = 0f;
         }
     }
 
     public void SpawnMeteor()
     {
-        GameObject temp = Instantiate(meteorPrefap, new Vector2(Random.Range(-6f, 6f), 8f), Quaternion.identity);
-        float randomSize = Random.Range(1f, 5f);
+        GameObject temp = Instantiate(meteorPrefap, new Vector2(UnityEngine.Random.Range(-6f, 6f), 8f), Quaternion.identity);
+        float randomSize = UnityEngine.Random.Range(1f, 5f);
         temp.GetComponent<MeteorCon>().SetMeteor(randomSize);
     }
 
